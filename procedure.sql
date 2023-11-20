a03e53348d216643fc323c18dcc4d425b4ff847b
Kauan G. Apolinário <kauan.g.apolinario@gmail.com>
2023-11-20 14:56:06 -0300

    Delete procedure.sql

--- procedure.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `AtualizaEstoque`(`cod` int, `qtde_comprada` int)
-BEGIN
-	
-	#select cod=cod_prod, quant=quantidade from vendas;
-    
-    update estoque set quantidade = quantidade - qtde_comprada where cod_prod = cod;
-END
